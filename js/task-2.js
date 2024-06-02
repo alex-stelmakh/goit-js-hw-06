ca2ead313206925948b8a0dbfa68a71299da765e
Alex <159806401+alex-stelmakh@users.noreply.github.com>
2024-06-02 23:38:15 +0300

    hw-6-js

---
+++ js/task-2.js
@@ -0,0 +1,35 @@
+class Storage {
+
+  #items;
+
+  constructor(items) {
+    this.#items = Array.from(items);
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    let indexOfItem = this.#items.indexOf(itemToRemove);
+    if (indexOfItem > -1) {
+      this.#items.splice(indexOfItem, 1);
+    }
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
